ebd2b328b255207ed04f7d51cde57db110e4eb12
Daniel ZHOU <danielzhou@Daniels-MacBook-Air.local>
2022-01-04 11:15:52 -0500

    added elo game

---
+++ Elo_Rating/Elo_Rating.py
@@ -0,0 +1,47 @@
+players = input("Please input player names: ").split()
+elo_dict = {}
+for player in players:
+    elo_dict.update({player: 1000})
+print(elo_dict)
+
+scores = input("Please input result here. (Template: User1 beats/ties/lost to User2): ")
+
+while scores != "exit":
+    score = scores.split()
+
+    if len(score) == 3:
+        person1 = score[0]
+        result = score[1]
+        person2 = score[2]
+
+    else:
+        person1 = score[0]
+        result = score[1]
+        person2 = score[3]
+
+    # Elo calculation based on 
+    # https://zh.wikipedia.org/wiki/%E7%AD%89%E7%BA%A7%E5%88%86
+    # and https://en.wikipedia.org/wiki/Elo_rating_system
+
+    if result == "beats":
+        new_1 = elo_dict[person1]+32*(1-(1/(1+10**((elo_dict[person2]-elo_dict[person1])/400))))
+        new_2 = elo_dict[person2]+32*(0-(1/(1+10**((elo_dict[person1]-elo_dict[person2])/400))))
+
+    elif result == "loses":
+        new_1 = elo_dict[person1]+32*(0-(1/(1+10**((elo_dict[person2]-elo_dict[person1])/400))))
+        new_2 = elo_dict[person2]+32*(1-(1/(1+10**((elo_dict[person1]-elo_dict[person2])/400))))
+
+    elif result == "ties":
+        new_1 = elo_dict[person1]+32*(0.5-(1/(1+10**((elo_dict[person2]-elo_dict[person1])/400))))
+        new_2 = elo_dict[person2]+32*(0.5-(1/(1+10**((elo_dict[person1]-elo_dict[person2])/400))))
+    
+    else:
+        print("Invalid Result; Please Try Again")
+        new_1 = elo_dict[person1]
+        new_2 = elo_dict[person2]
+
+    elo_dict.update({person1: int(round(new_1,0))})
+    elo_dict.update({person2: int(round(new_2,0))})
+    print(elo_dict)
+
+    scores = input("Please input result here. (Template: User1 beats/ties/lost to User2): ")
--- money_game.py
+++ money_game.py
@@ -4,12 +4,12 @@ import random
 names = input("Enter your names: ")
 names = names.split()
 personA, personB = names
-currency_arr = [600,600]
+currency_arr = [2000,2000]
 count_arr = [0,0]
 n = random.randrange(0, 2)
 l0 = []
 
-for _ in range(200):
+for _ in range(500):
     l0.append(0)
 
 yearly_earnings_arr = [0,0]
@@ -22,38 +22,44 @@ while currency_arr[0] < 10000 and currency_arr[1] < 10000:
     print(f"Your currency: {currency_arr[k]} dollars")
     choice = input("What would you like to do? ")
     if choice == "beg":
-        currency_arr[k] += random.randrange(0, 50)
+        currency_arr[k] += random.randrange(0, 200)
     elif choice == "start shop":
         print("cost: 500 dollars")
         if currency_arr[k] < 500:
             print("You don't have enough money")
         elif count_arr[k] == 0:
             currency_arr[k] -= 500
-            yearly_earnings_arr[k] = random.randrange(50,200)
+            yearly_earnings_arr[k] = random.randrange(200,400)
             count_arr[k] = 1
             print(f"Congradulations! You will earn {yearly_earnings_arr[k]} dollars every year from your shop!")
         else:
             print("You already have a shop")
+            n -= 1
     elif choice == "rob":
-        amount = int(input("How much? (Amount must be greater or equal to 50 dollars) "))
-        l = l0
-        for _ in range(amount):
-            l.append(1)
-        random.shuffle(l)
-        a = random.randrange(0,len(l))
-        if l[a] % 2 == 1:
-            print("You got caught")
-            x = random.randrange(amount-50,amount+100)
-            print(f"fine: {x} dollars")
-            print(f"You gave {names[1-k]} {x} dollars")
-            currency_arr[k] -= x
-            currency_arr[1-k] += x
-        else:
-            print("That was successful!")
-            currency_arr[k] += amount
+        amount = int(input("How much? "))
+        if amount > currency_arr[k]:
+            print("amount too large, try again")
+            n -= 1
+        else:    
+            l = l0
+            for _ in range(amount):
+                l.append(1)
+            random.shuffle(l)
+            print(l)
+            a = random.randrange(0,len(l))
+            if l[a] == 1:
+                print("You got caught")
+                x = random.randrange(amount,amount+150)
+                print(f"fine: {x} dollars")
+                print(f"You gave {names[1-k]} {x} dollars")
+                currency_arr[k] -= x
+                currency_arr[1-k] += x
+            else:
+                print("That was successful!")
+                currency_arr[k] += amount
     else:
         print("Wrong input. Please try again")
-        n = n-1
+        n -= 1
 
     print(f"Your currency: {currency_arr[k]} dollars\n")
     n += 1
